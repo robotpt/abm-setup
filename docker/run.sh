17c423caf483f97a24470411539bc2ac735c16e1
Audrow <audrow.nash@gmail.com>
2020-02-16 19:37:15 -0800

    Create a run script with setup, run, stop, and debug options

--- docker/run.sh
+++ docker/run.sh
@@ -4,9 +4,31 @@
 FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $FILE_DIR
 
-# share display on ubuntu platforms
-xhost +local:
-
-# build the docker container and run the default command 
-# set in the Dockerfile or docker-compose.yaml file
+SERVICE_NAME="abm_interaction"
 ROS_MASTER_URI=$ROS_MASTER_URI docker-compose up --build
+case "$1" in 
+	setup )
+		echo "Calling setup script"
+		ROS_MASTER_URI=$ROS_MASTER_URI docker-compose run $SERVICE_NAME bash -c "source ~/ws/catkin_ws/devel/setup.bash && roslaunch abm_interaction setup_abm_interaction.launch"
+		;;
+	run )
+		echo "Running in the background"
+		ROS_MASTER_URI=$ROS_MASTER_URI docker-compose run -d $SERVICE_NAME bash -c "source ~/ws/catkin_ws/devel/setup.bash && roslaunch abm_interaction qt_abm_interaction.launch"
+		;;
+	stop )
+		read -p "Are you sure that you want to stop the running containers?" -n 1 -r
+		echo    # (optional) move to a new line
+		if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+			docker-compose down
+		fi
+		ROS_MASTER_URI=$ROS_MASTER_URI docker-compose down
+		;;
+	debug )
+		echo "Starting debug"
+		xhost +local: # share display on ubuntu platforms
+		ROS_MASTER_URI=$ROS_MASTER_URI docker-compose run $SERVICE_NAME bash -c "terminator -e \"echo 'Entering Docker Container...' && bash\""
+		;;
+	* )
+		echo "Please enter 'setup', 'run', 'stop', or 'debug'"
+		;;
+esac
