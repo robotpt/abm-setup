d1dc34bf18290893ac740b3862c449458715a8cc
Ali Paikan <ali.paikan@luxai.com>
2020-03-02 18:54:59 +0100

    Add audio common to setup script

--- scripts/pi_setup.bash
+++ scripts/pi_setup.bash
@@ -6,10 +6,11 @@ sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
 # add the new key
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
-sudo apt-get update -y && apt-get upgrade -y
+sudo apt-get update -y && sudo apt-get upgrade -y
 
 sudo apt-get install -y \
     python-pyaudio \
     luakit \
+    ros-${ROS_DISTRO}-audio-common \
     libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
     dphys-swapfile
