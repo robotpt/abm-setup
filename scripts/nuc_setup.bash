dc502c490f1be241be51ebd0d0a4dbac0b6aadaf
Audrow <audrow.nash@gmail.com>
2020-03-02 11:28:43 -0800

    Make update work in NUC's script

--- scripts/nuc_setup.bash
+++ scripts/nuc_setup.bash
@@ -6,4 +6,4 @@ sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
 # add the new key
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
-sudo apt-get update -y && apt-get upgrade -y
+sudo apt-get update -y && sudo apt-get upgrade -y
