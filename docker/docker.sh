5757a3fe704cc3e071f8db22ad9c3c83948582ab
Audrow <audrow.nash@gmail.com>
2020-05-06 10:44:00 -0700

    Make docker.sh have default argument

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,9 +4,16 @@
 FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $FILE_DIR
 
+if [ -z ${1+x} ]; 
+then 
+	MODE="debug";
+else
+	MODE=$1;
+fi
+
 SERVICE_NAME="abm_interaction"
 ROS_MASTER_URI=$ROS_MASTER_URI docker-compose up --build
-case "$1" in 
+case "$MODE" in 
 	setup )
 		echo "Calling setup script"
 		ROS_MASTER_URI=$ROS_MASTER_URI docker-compose run $SERVICE_NAME bash -c "source ~/ws/catkin_ws/devel/setup.bash && roslaunch abm_interaction setup_abm_interaction.launch"
